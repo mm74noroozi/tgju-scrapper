afdf7a0c5ad373b55f130e45c1a556a7006c9ee0
mohammad <mm.74noroozi@gmail.com>
2020-02-04 18:43:54 +0330

    Datetime module added

--- scrap.js
+++ scrap.js
@@ -1,11 +1,17 @@
 var request = require('request');
 var DomParser = require('dom-parser');
+var dateTime = require('node-datetime');
 var parser = new DomParser();
 
-request('https://www.tgju.org/',function(err,res,body){
-    var document = parser.parseFromString(html);
-    console.log(dom.getElementById('server-time').innerHTML);
-})
+var dt = dateTime.create();
+var formatted = dt.format('Y-m-d H:M:S');
+console.log(formatted);
+
+// request('https://www.tgju.org/',function(err,res,body){
+//     var document = parser.parseFromString(body);
+//     console.log(document.getElementById('server-time').getAttribute('data-value'));
+//     // console.log(body)
+// })
 // request("https://call2.tgju.org/ajax.json?" + data_revision + "-" + t + "-" + make_random_str(20))
 
 // function make_random_str(rand_limit) {
