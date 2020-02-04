9f10ae8c92b7c0646c7fb94e0d8cc669e6f5a6b5
mohammad <mm.74noroozi@gmail.com>
2020-02-04 19:01:45 +0330

    one time get json completed

--- scrap.js
+++ scrap.js
@@ -3,22 +3,26 @@ var DomParser = require('dom-parser');
 var dateTime = require('node-datetime');
 var parser = new DomParser();
 
-var dt = dateTime.create();
-var formatted = dt.format('Y-m-d H:M:S');
-console.log(formatted);
 
-// request('https://www.tgju.org/',function(err,res,body){
-//     var document = parser.parseFromString(body);
-//     console.log(document.getElementById('server-time').getAttribute('data-value'));
-//     // console.log(body)
-// })
-// request("https://call2.tgju.org/ajax.json?" + data_revision + "-" + t + "-" + make_random_str(20))
 
-// function make_random_str(rand_limit) {
-//     var text = "";
-//     var possible =
-//       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-//     for (var i = 0; i < rand_limit; i++)
-//       text += possible.charAt(Math.floor(Math.random() * possible.length));
-//     return text;
-// }
+givePrices()
+
+
+
+function givePrices(){
+    var dt = dateTime.create();
+    var data_revision = dt.format('YmdH');
+    var t = dt.format('YmdHMS');
+    request("https://call2.tgju.org/ajax.json?" + data_revision + "-" + t + "-" + make_random_str(20),function(err,res,body){
+        console.log(JSON.stringify(JSON.parse(body),null,2));
+    })
+}
+
+function make_random_str(rand_limit) {
+    var text = "";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < rand_limit; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
